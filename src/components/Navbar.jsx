bac80d542cb807e4c5cc1716446919f5e20b5982
Esteban Pelutiero <estebanpelutiero@gmail.com>
2024-01-29 16:38:14 -0300

    works section added with new cards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,181 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
-import {
-  Collapse,
-  Typography,
-  IconButton,
-} from "@material-tailwind/react";
+import { Collapse, Typography, IconButton } from "@material-tailwind/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const WINDOW_INNERWIDTH = window.innerWidth;
 
-const NavList = ({closeMenu}) => {
-
-  const handleClose = () => {
-    closeMenu();
-  }
-
-  return (
-    <>
-      <ul className="lg:hidden z-10 mt-6 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-
-        {/* Home */}
-
-        <Typography
-          as="li"
-          variant="small"
-          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
-        >
-          <Link
-            smooth={true}
-            spy={true}
-            to="home"
-            duration={1300}
-            onClick={handleClose}
-            className="cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
-          >
-            Home
-          </Link>
-        </Typography>
-
-        {/* About */}
-
-        <Typography
-          as="li"
-          variant="small"
-          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
-        >
-          <Link
-            smooth={true}
-            spy={true}
-            to="about"
-            duration={1300}
-            onClick={() => handleClose()}
-            className="cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
-          >
-            About
-          </Link>
-        </Typography>
-
-        {/* Work */}
-
-        <Typography
-          as="li"
-          variant="small"
-          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
-        >
-          <Link
-            smooth={true}
-            spy={true}
-            to="work"
-            duration={1300}
-            onClick={() => handleClose()}
-            className="cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
-          >
-            Projects
-          </Link>
-        </Typography>
-
-        {/* Contact */}
-
-        <Typography
-          as="li"
-          variant="small"
-          className="flex justify-end lg:pl-4 lg:py-3 pt-3 pb-4"
-        >
-          <Link
-            smooth={true}
-            spy={true}
-            to="contact"
-            duration={1300}
-            onClick={() => handleClose()}
-            className="cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
-          >
-            Contact
-          </Link>
-        </Typography>
-      </ul>
-
-      {/* desktop */}
-
-      <ul className="hidden z-10 mt-6 lg:flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-
-        <Typography
-          as="li"
-          variant="small"
-          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
-        >
-          <Link
-            smooth={true}
-            spy={true}
-            to="home"
-            duration={1300}
-            className="hover:text-purple ease-linear duration-100 cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
-          >
-            Home
-          </Link>
-        </Typography>
-
-        {/* About */}
-
-        <Typography
-          as="li"
-          variant="small"
-          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
-        >
-          <Link
-            smooth={true}
-            spy={true}
-            to="about"
-            duration={1300}
-            className="hover:text-purple ease-linear duration-100 cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
-          >
-            About
-          </Link>
-        </Typography>
-
-        {/* Work */}
-
-        <Typography
-          as="li"
-          variant="small"
-          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
-        >
-          <Link
-            smooth={true}
-            spy={true}
-            to="work"
-            duration={1300}
-            className="hover:text-purple ease-linear duration-100 cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
-          >
-            Projects
-          </Link>
-        </Typography>
-
-        {/* Contact */}
-
-        <Typography
-          as="li"
-          variant="small"
-          className="flex justify-end lg:pl-4 lg:py-3 pt-3 pb-5"
-        >
-          <Link
-            smooth={true}
-            spy={true}
-            to="contact"
-            duration={1300}
-            className="hover:text-purple ease-linear duration-100 cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
-          >
-            Contact
-          </Link>
-        </Typography>
-      </ul>
-    </>
-  );
-};
-
 const Navbar = () => {
-
   const [openNav, setOpenNav] = useState(false);
 
   const handleWindowResize = () =>
@@ -205,25 +35,29 @@ const Navbar = () => {
           >
             <div>
               <Link
-              to="home"
-              smooth={true}
-              offset={-80}
-              className="hidden lg:inline cursor-pointer"
-              duration={1300}
+                to="home"
+                smooth={true}
+                offset={-80}
+                className="hidden lg:inline cursor-pointer"
+                duration={1300}
               >
-                <h1 className="text-white text-[17px] font-poppins font-semibold hover:text-purple ease-linear duration-100">Esteban.dev</h1>
+                <h1 className="text-white text-[17px] font-poppins font-semibold hover:text-purple ease-linear duration-100">
+                  Esteban.dev
+                </h1>
               </Link>
             </div>
             <div className="">
               <Link
-              to="home"
-              smooth={true}
-              offset={-80}
-              className="lg:hidden inline cursor-pointer"
-              duration={1300}
-              onClick={() => closeMenu()}
+                to="home"
+                smooth={true}
+                offset={-80}
+                className="lg:hidden inline cursor-pointer"
+                duration={1300}
+                onClick={() => closeMenu()}
               >
-                <h1 className="text-white text-[17px] font-poppins font-semibold hover:text-purple ease-linear duration-100">Esteban.dev</h1>
+                <h1 className="text-white text-[17px] font-poppins font-semibold hover:text-purple ease-linear duration-100">
+                  Esteban.dev
+                </h1>
               </Link>
             </div>
           </Typography>
@@ -244,13 +78,168 @@ const Navbar = () => {
           </IconButton>
         </div>
         <Collapse open={openNav}>
-          <NavList closeMenu={closeMenu}/>
+          <NavList closeMenu={closeMenu} />
         </Collapse>
       </nav>
     </div>
   );
 };
 
+const NavList = ({ closeMenu }) => {
+  const handleClose = () => {
+    closeMenu();
+  };
 
+  return (
+    <>
+      <ul className="lg:hidden z-10 mt-6 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+        {/* Home */}
+
+        <Typography
+          as="li"
+          variant="small"
+          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
+        >
+          <Link
+            smooth={true}
+            spy={true}
+            to="home"
+            onClick={handleClose}
+            className="cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
+          >
+            Home
+          </Link>
+        </Typography>
+
+        {/* About */}
+
+        <Typography
+          as="li"
+          variant="small"
+          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
+        >
+          <Link
+            smooth={true}
+            spy={true}
+            to="about"
+            onClick={() => handleClose()}
+            delay={0}
+            className="cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
+          >
+            About
+          </Link>
+        </Typography>
+
+        {/* Work */}
+
+        <Typography
+          as="li"
+          variant="small"
+          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
+        >
+          <Link
+            smooth={true}
+            spy={true}
+            to="work"
+            onClick={() => handleClose()}
+            delay={0}
+            className="cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
+          >
+            Projects
+          </Link>
+        </Typography>
+
+        {/* Contact */}
+
+        <Typography
+          as="li"
+          variant="small"
+          className="flex justify-end lg:pl-4 lg:py-3 pt-3 pb-4"
+        >
+          <Link
+            smooth={true}
+            spy={true}
+            to="contact"
+            onClick={() => handleClose()}
+            delay={0}
+            className="cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
+          >
+            Contact
+          </Link>
+        </Typography>
+      </ul>
+
+      {/* desktop */}
+
+      <ul className="hidden z-10 mt-6 lg:flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+        <Typography
+          as="li"
+          variant="small"
+          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
+        >
+          <Link
+            smooth={true}
+            spy={true}
+            to="home"
+            className="hover:text-purple ease-linear duration-100 cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
+          >
+            Home
+          </Link>
+        </Typography>
+
+        {/* About */}
+
+        <Typography
+          as="li"
+          variant="small"
+          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
+        >
+          <Link
+            smooth={true}
+            spy={true}
+            to="about"
+            className="hover:text-purple ease-linear duration-100 cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
+          >
+            About
+          </Link>
+        </Typography>
+
+        {/* Work */}
+
+        <Typography
+          as="li"
+          variant="small"
+          className="flex justify-end lg:px-4 py-3 border-b-[1px] border-white/50 lg:border-none"
+        >
+          <Link
+            smooth={true}
+            spy={true}
+            to="work"
+            className="hover:text-purple ease-linear duration-100 cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
+          >
+            Projects
+          </Link>
+        </Typography>
+
+        {/* Contact */}
+
+        <Typography
+          as="li"
+          variant="small"
+          className="flex justify-end lg:pl-4 lg:py-3 pt-3 pb-5"
+        >
+          <Link
+            smooth={true}
+            spy={true}
+            to="contact"
+            className="hover:text-purple ease-linear duration-100 cursor-pointer flex items-center font-poppins text-white font-semibold text-base transition-colors"
+          >
+            Contact
+          </Link>
+        </Typography>
+      </ul>
+    </>
+  );
+};
 
 export default Navbar;
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "./Layout";
 import Garden from "../assets/images/clean-garden.png";
-import Oss from '../assets/images/oss-web.png';
+import Oss from "../assets/images/oss-web.png";
 import { FiGithub } from "react-icons/fi";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
@@ -20,7 +20,7 @@ const Work = () => {
       <section className="flex flex-col gap-16">
         {/* WORK 1 */}
 
-        <div className="group flex flex-col-reverse gap-8 lg:flex-row items-center justify-between rounded-lg px-4 py-6 lg:py-4 bg-black/20 backdrop-blur-lg shadow-[0_8px_30px_rgb(0,0,0,0.3)] border border-white/50">
+        <div className="group flex flex-col-reverse gap-8 lg:flex-row items-center justify-between rounded-lg p-4 bg-black/20 backdrop-blur-lg shadow-[0_8px_30px_rgb(0,0,0,0.3)] border border-white/20">
           <article className="lg:w-[40%] text-white font-poppins">
             <div className="w-full flex flex-col items-center text-center">
               <div className="mb-5">
@@ -29,34 +29,58 @@ const Work = () => {
                 </h3>
               </div>
 
-              <div className="lg:w-[80%] mb-8">
+              <div className="lg:w-[80%] mb-6">
                 <p className="">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                  amet, assumenda eligendi, culpa veritatis dolor blanditiis enim
-                  dicta perferendis fugiat voluptate iusto explicabo libero harum
-                  ea? Illo eveniet culpa eaque.
+                  The design phase focuses on simplicity, employing a clean
+                  color palette and intuitive navigation. Imagery, carefully
+                  selected to resonate with cleanliness and professionalism,
+                  plays a pivotal role. A responsive layout ensures a seamless
+                  user experience across various devices.
                 </p>
               </div>
 
-              <div className="w-full lg:w-[70%] flex items-center justify-around">
-                <button className="group/btn py-2 px-4 border-l border-r border-purple rounded-lg ">
-                  <a className="flex items-center gap-2 text-sm" href="#">
-                    Code <FiGithub className="group-hover/btn:-translate-y-0.5 ease-in-out duration-[200ms]" size={16} color="white" />
+              <div className="w-[80%] lg:w-[70%] flex items-center justify-around border-t border-white/10 pt-4">
+                <button className="group/btn py-2 px-4 rounded-lg ">
+                  <a
+                    className="group/anchor flex items-center gap-2 text-sm"
+                    href="https://github.com/estebanPelutiero/clean-and-garden"
+                    target="_blank"
+                    rel="noopener noreferer"
+                  >
+                    <span className="bg-left-bottom bg-gradient-to-r from-purple to-purple bg-[length:0%_2px] bg-no-repeat group-hover/anchor:bg-[length:100%_2px] transition-all duration-300 ease-out pb-1">
+                      Code
+                    </span>{" "}
+                    <FiGithub
+                      className="group-hover/btn:-translate-y-0.5 ease-in-out duration-[200ms] mb-1"
+                      size={16}
+                      color="white"
+                    />
                   </a>
                 </button>
-                <button className="group/btn py-2 px-4 border-l border-r border-purple rounded-lg">
-                  <a className="flex items-center gap-2 text-sm" href="#">
-                    Visit <MdOutlineKeyboardArrowRight className="-mr-2 -ml-1 group-hover/btn:translate-x-0.5 ease-in-out duration-[200ms]" size={20} color="white" />
+                <button className="group/btn py-2 px-4  rounded-lg">
+                  <a
+                    className="group/anchor flex items-center gap-2 text-sm"
+                    href="https://cgarden.com.ar/"
+                    target="_blank"
+                    rel="noopener noreferer"
+                  >
+                    <span className="bg-left-bottom bg-gradient-to-r from-purple to-purple bg-[length:0%_2px] bg-no-repeat group-hover/anchor:bg-[length:100%_2px] transition-all duration-300 ease-out pb-1">
+                      Visit
+                    </span>{" "}
+                    <MdOutlineKeyboardArrowRight
+                      className="-mr-2 -ml-1 group-hover/btn:translate-x-0.5 ease-in-out duration-[200ms] mb-1"
+                      size={20}
+                      color="white"
+                    />
                   </a>
                 </button>
               </div>
             </div>
-
           </article>
 
-          <figure className="lg:w-[53%] rounded-lg max-h-60 lg:max-h-80 overflow-hidden">
+          <figure className="lg:w-[53%] rounded-lg max-h-60 md:max-h-80 overflow-hidden">
             <img
-              className="group-hover:-translate-y-[84.9%] ease-linear duration-[6000ms]"
+              className="lg:group-hover:-translate-y-[84.9%] ease-in-out duration-[8000ms]"
               src={Garden}
               alt="Clean & Garden | Servicio de limpieza integral"
             />
@@ -65,7 +89,7 @@ const Work = () => {
 
         {/* WORK 2 */}
 
-        <div className="group flex flex-col-reverse gap-8 lg:flex-row-reverse items-center justify-between rounded-lg px-4 py-6 lg:py-4 bg-black/20 backdrop-blur-lg shadow-[0_8px_30px_rgb(0,0,0,0.3)] border border-white/50">
+        <div className="group flex flex-col-reverse gap-8 lg:flex-row items-center justify-between rounded-lg p-4 bg-black/20 backdrop-blur-lg shadow-[0_8px_30px_rgb(0,0,0,0.3)] border border-white/20">
           <article className="lg:w-[40%] text-white font-poppins">
             <div className="w-full flex flex-col items-center text-center">
               <div className="mb-5">
@@ -74,45 +98,63 @@ const Work = () => {
                 </h3>
               </div>
 
-              <div className="lg:w-[80%] mb-8">
+              <div className="lg:w-[80%] mb-6">
                 <p className="">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
-                  amet, assumenda eligendi, culpa veritatis dolor blanditiis enim
-                  dicta perferendis fugiat voluptate iusto explicabo libero harum
-                  ea? Illo eveniet culpa eaque.
+                  Designing a sleek landing page for a modern digital agency.
+                  Emphasizing contemporary aesthetics, it showcases innovation
+                  through engaging visuals and intuitive navigation,
+                  communicating professionalism and expertise.
                 </p>
               </div>
 
-              <div className="w-full lg:w-[70%] flex items-center justify-around">
-                <button className="group/btn py-2 px-4 border-l border-r border-purple rounded-lg ">
-                  <a className="flex items-center gap-2 text-sm" href="#">
-                    Code <FiGithub className="group-hover/btn:-translate-y-0.5 ease-in-out duration-[200ms]" size={16} color="white" />
+              <div className="w-[80%] lg:w-[70%] flex items-center justify-around border-t border-white/10 pt-4">
+                <button className="group/btn py-2 px-4 rounded-lg ">
+                  <a
+                    className="group/anchor flex items-center gap-2 text-sm"
+                    href="https://github.com/estebanPelutiero/oss-web-studio"
+                    target="_blank"
+                    rel="noopener noreferer"
+                  >
+                    <span className="bg-left-bottom bg-gradient-to-r from-purple to-purple bg-[length:0%_2px] bg-no-repeat group-hover/anchor:bg-[length:100%_2px] transition-all duration-300 ease-out pb-1">
+                      Code
+                    </span>{" "}
+                    <FiGithub
+                      className="group-hover/btn:-translate-y-0.5 ease-in-out duration-[200ms] mb-1"
+                      size={16}
+                      color="white"
+                    />
                   </a>
                 </button>
-                <button className="group/btn py-2 px-4 border-l border-r border-purple rounded-lg">
-                  <a className="flex items-center gap-2 text-sm" href="#">
-                    Visit <MdOutlineKeyboardArrowRight className="-mr-2 -ml-1 group-hover/btn:translate-x-0.5 ease-in-out duration-[200ms]" size={20} color="white" />
+                <button className="group/btn py-2 px-4  rounded-lg">
+                  <a
+                    className="group/anchor flex items-center gap-2 text-sm"
+                    href="https://osswebstudio.tech/"
+                    target="_blank"
+                    rel="noopener noreferer"
+                  >
+                    <span className="bg-left-bottom bg-gradient-to-r from-purple to-purple bg-[length:0%_2px] bg-no-repeat group-hover/anchor:bg-[length:100%_2px] transition-all duration-300 ease-out pb-1">
+                      Visit
+                    </span>{" "}
+                    <MdOutlineKeyboardArrowRight
+                      className="-mr-2 -ml-1 group-hover/btn:translate-x-0.5 ease-in-out duration-[200ms] mb-1"
+                      size={20}
+                      color="white"
+                    />
                   </a>
                 </button>
               </div>
             </div>
-
           </article>
 
-          <figure className="lg:w-[53%] rounded-lg max-h-60 lg:max-h-80 overflow-hidden">
+          <figure className="lg:w-[53%] rounded-lg max-h-60 md:max-h-80 overflow-hidden">
             <img
-              className="group-hover:-translate-y-[84.9%] ease-linear duration-[6000ms]"
+              className="lg:group-hover:-translate-y-[84.9%] ease-in-out duration-[8000ms]"
               src={Oss}
               alt="Clean & Garden | Servicio de limpieza integral"
             />
           </figure>
         </div>
-
-        
-
-       
       </section>
-
     </Layout>
   );
 };
