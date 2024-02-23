a310f04d64ab0ce6c3bf7f43112dc44b42015992
Esteban Pelutiero <estebanpelutiero@gmail.com>
2024-02-23 17:18:37 -0300

    chaged file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
-import { LanguageProvider } from "./languageContext.jsx";
+import { LanguageProvider } from "./LanguageContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
