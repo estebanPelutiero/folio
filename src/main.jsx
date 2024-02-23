3f5d356135fa1fe7fa43e8110b0b54090ef71b19
Esteban Pelutiero <estebanpelutiero@gmail.com>
2024-02-23 17:09:30 -0300

    change in wrong named file in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
-import { LanguageProvider } from "./languageContext";
+import { LanguageProvider } from "./languageContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
